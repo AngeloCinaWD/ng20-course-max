4b2bff884efd80c20f0c14d149d247c1e07f133f
AngeloCinaWD <angelo.cina@yahoo.it>
2025-10-13 21:17:21 +0200

    29. We Need More Flexible Components!

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -10,29 +10,12 @@ const randomIndex: number = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // con l'implementazione dei signals (da ng16 e resi stabili da ng17) da parte di angular c'è la volontà di non utilizzare più zone.js per la change detection
-  // un signal è un container con un valore iniziale, quando cambia quello che c'è nel container vengono aggiornati tutti i punti dell'app dove quel container è utilizzato, è una funzione e così va utilizzata
-  // selectedUser: { id: string; name: string; avatar: string } = DUMMY_USERS[randomIndex];
-  selectedUser: WritableSignal<{ id: string; name: string; avatar: string }> = signal(
-    DUMMY_USERS[randomIndex]
-  );
-
-  // creo un getter con la keyword get
-  // una funzione che restituisce qualcosa e viene utilizzata nel template come una semplice proprietà, non va eseguita
-  get imagePath(): string {
-    // return `/assets/users/${this.selectedUser.avatar}`;
-    return `/assets/users/${this.selectedUser().avatar}`;
-  }
-
-  // invece del get utilizzo le computed signals, funzioni che restituiscono qualcosa dipendentemente ad un signal che cambia
-  // cambia lo user e viene ritornato il nuovo valore di avatar
-  imagePathComputed: Signal<string> = computed(() => `/assets/users/${this.selectedUser().avatar}`);
-
-  // questo metodo aggiorna la UI, cambia il valore di uno stored data, la property selectedUser
-  // angular si accorge di questi cambiamenti tramite la libreria zone.js
-  onSelectedUser(): void {
-    const randomIndex: number = Math.floor(Math.random() * DUMMY_USERS.length);
-    // this.selectedUser = DUMMY_USERS[randomIndex];
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  }
+  // selectedUser: WritableSignal<{ id: string; name: string; avatar: string }> = signal(
+  //   DUMMY_USERS[randomIndex]
+  // );
+  // imagePathComputed: Signal<string> = computed(() => `/assets/users/${this.selectedUser().avatar}`);
+  // onSelectedUser(): void {
+  //   const randomIndex: number = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // }
 }
