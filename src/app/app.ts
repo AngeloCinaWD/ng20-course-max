91a376a1c350a4bbe96df2bb5630aaa254279f40
AngeloCinaWD <angelo.cina@yahoo.it>
2025-10-13 19:33:52 +0200

    19. Preparing User Data (To Output Dynamic Content)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header.component/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
