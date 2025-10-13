c0cf0846b950d113fdc2f13ef5e360024ff35677
AngeloCinaWD <angelo.cina@yahoo.it>
2025-10-13 21:53:18 +0200

    32. Using Signal Inputs

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, input, Input, InputSignal, Signal } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,15 +7,20 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // passo i dati al componente dall'esterno, dal componente padre
-  // creo proprietà con il decorator @Input
-  // col ! convinco typescript che quell'input property riceverà un valore, se passo un oggetto di configurazione all'Input con la proprietà required true sarò costretto a passare un valore dal parent altrimenti avrò un errore. Senza questa opzione potrei non passare nulla
-  @Input({ required: true }) avatar!: string;
-  @Input() name!: string;
+  // l'Input decorator può essere sostituito dal più moderno input() signal
+  // un InputSignal è readonly quindi non si possono utilizzare metodi come set o update
+  // @Input({ required: true }) avatar!: string;
+  // @Input() name!: string;
 
-  get imagePath(): string {
-    return `/assets/users/${this.avatar}`;
-  }
+  // avatar = input<string>();
+  avatar: InputSignal<string> = input.required();
+  name: InputSignal<string> = input.required();
+
+  // get imagePath(): string {
+  //   return `/assets/users/${this.avatar}`;
+  // }
+  // dato che le properties sono InputSignal posso utilizzare una computed signal per restituire la src delle immagini
+  imagePath: Signal<string> = computed(() => `/assets/users/${this.avatar()}`);
 
   onSelectedUser(): void {}
 }
