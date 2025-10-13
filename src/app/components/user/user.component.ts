77e77885cdd90d3e3ab79343a0543710fa055398
AngeloCinaWD <angelo.cina@yahoo.it>
2025-10-13 21:55:52 +0200

    33. We Need Custom Events!

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -9,18 +9,18 @@ import { Component, computed, input, Input, InputSignal, Signal } from '@angular
 export class UserComponent {
   // l'Input decorator può essere sostituito dal più moderno input() signal
   // un InputSignal è readonly quindi non si possono utilizzare metodi come set o update
-  // @Input({ required: true }) avatar!: string;
-  // @Input() name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input() name!: string;
 
   // avatar = input<string>();
-  avatar: InputSignal<string> = input.required();
-  name: InputSignal<string> = input.required();
+  // avatar: InputSignal<string> = input.required();
+  // name: InputSignal<string> = input.required();
 
-  // get imagePath(): string {
-  //   return `/assets/users/${this.avatar}`;
-  // }
+  get imagePath(): string {
+    return `/assets/users/${this.avatar}`;
+  }
   // dato che le properties sono InputSignal posso utilizzare una computed signal per restituire la src delle immagini
-  imagePath: Signal<string> = computed(() => `/assets/users/${this.avatar()}`);
+  // imagePath: Signal<string> = computed(() => `/assets/users/${this.avatar()}`);
 
   onSelectedUser(): void {}
 }
