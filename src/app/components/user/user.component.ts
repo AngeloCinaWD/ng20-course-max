eaf2382046bb2bbe5635cd36f621efbd1420fa70
AngeloCinaWD <angelo.cina@yahoo.it>
2025-10-13 20:46:46 +0200

    26. Managing State & Changing Data

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -19,6 +19,7 @@ export class UserComponent {
   }
 
   onSelectedUser(): void {
-    console.log('Clicked!!!');
+    const randomIndex: number = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
